c70602eb9efa67b26f9491b71846ef7d881f1fc2
makdc <malikdc96@gmail.com>
2025-04-24 15:11:21 -0700

    Setting up starter files, unable to run main.sh

---
+++ main.sh
@@ -0,0 +1,1 @@
+﻿python3 src/main.py
---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import *
+
+# hello world
+
+def main():
+    TextNode("This is some anchor text", link, "https://www.boot.dev")
+    return
+
+
+
+main()
