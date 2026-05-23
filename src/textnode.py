0d1c5235a7d251ceec6f6e8d592cf6a92a796604
makdc <malikdc96@gmail.com>
2026-05-23 10:09:50 -0700

    Fix props for LINK and IMAGE in text_node_to_html_node; add split_nodes_delimiter function

--- src/textnode.py
+++ src/textnode.py
@@ -36,8 +36,25 @@ def text_node_to_html_node(text_node):
         case TextType.CODE:
             return LeafNode(tag='code', value=text_node.text)
         case TextType.LINK:
-            return LeafNode(tag='a', value=text_node.text, props='href')
+            return LeafNode(tag='a', value=text_node.text, props={"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode(tag='img', value="", props=[src, alt])
+            return LeafNode(tag='img', value="", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise Exception(f"invalid entry for text node {text_node.text_type}")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    for on in old_nodes:
+        print(on)
+        if delimiter in on:
+            new_lst = []
+            on_split_text = on.split('`')
+            for ons in on_split_text:
+                print(ons)
+                new_lst.append(TextNode(on_split_text[ons], TextType.TEXT))
+                #ons1 = TextNode(on_split_text[1], TextType.CODE)
+                #ons2 = TextNode(on_split_text[2], TextType.TEXT)
+    return new_lst       
+                
+                
+            
+            
